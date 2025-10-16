452556c1fe001defe821ced709a21587aff91419
Darius Pilakan <83500619+Pkorirdarius@users.noreply.github.com>
2025-10-16 10:58:20 +0300

    Dynamic_routing

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def index(request,age):
+    return HttpResponse("Hi, my age  is $age :" %age)
--- web_project/urls.py
+++ web_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from hello import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("hello.urls"))
+    path("", include("hello.urls")),
+    path("<age>/",views.index,name="index")
 ]
